82764ce84e89e15eb1352c4a352f2ba3423c64fd
yokohamasoken <info@yokohamasoken.jp>
2026-09-16 14:30:42 +0900

    feat: ?????shell forest????????

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const userLastProject = new Map();
 const SENDER_CLIENT_MAP = [
   { keywords: ['一梧works', '一梧', 'ichigo'], clientId: 'Z2lkOi8vYXBpL0NsaWVudC82ODAwNDA' }, // 株式会社一梧works
   { keywords: ['三心', 'sanshin', '現場発注三心'], clientId: 'Z2lkOi8vYXBpL0NsaWVudC82ODAzOTc' }, // 三心建設
-  { keywords: ['shell forest', 'シェルフォレスト', '現場発注shell'], clientId: 'Z2lkOi8vYXBpL0NsaWVudC82ODIxNTU' }, // shell forest
+  { keywords: ['shell forest', 'シェルフォレスト', '現場発注shell', '島田一樹', '島田', 'shimada'], clientId: 'Z2lkOi8vYXBpL0NsaWVudC82ODIxNTU' }, // shell forest
   { keywords: ['峯尾', 'みねお', 'mineo'], clientId: 'Z2lkOi8vYXBpL0NsaWVudC82ODU5MzA' }, // 株式会社 BEN.MINE
   { keywords: ['佐藤', 'さとう', 'sato'], clientId: 'Z2lkOi8vYXBpL0NsaWVudC83NDcwODk' }, // 株式会社トライコーポレーション
 ];
